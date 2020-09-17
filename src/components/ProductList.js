3a103083cb1fc740db337c4b1325a95e4485412c
iclectic <40839135+iclectic@users.noreply.github.com>
2020-09-17 03:17:53 +0100

    Update ProductList.js

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -25,7 +25,7 @@ export default class ProductList extends Component {
                 </div>
                 </div>            
                </React.Fragment>
-                /* <Product /> */
+               
            </div>
         );
     }
