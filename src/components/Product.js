028b88e7fab502b443165de08e412aca28e11808
iclectic <tansonestost@yahoo.com>
2020-09-26 04:12:10 +0100

    E-Commerce Website in ReactJs

--- src/components/Product.js
+++ src/components/Product.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import styled from "styled-components";
 import{Link} from "react-router-dom";
 import {ProductConsumer} from "../context";
+import PropTypes from 'prop-types';
 
 
 export default class Product extends Component {
@@ -11,14 +12,13 @@ export default class Product extends Component {
             <ProductWrapper className="col-9 mx-auto col-md-6 col-lg-3 my-3">
             
              <div className="card">
-                 <div className="img-container p-5" onClick={console.log('you clicked on me on the image container')}>
+                 <ProductConsumer>
+                     {(value) => ( 
+                  <div className="img-container p-5" onClick={() => value.handleDetail(id)}>
                      <Link to="/details">
                          <img src={img} alt="product" className="card-img-top"/>
                      </Link>
-                     <button className="cart-btn" disabled={inCart ? true :
-                     false} onClick={()=>{console.log('added to the cart');
-                     }}
-                     >
+                     <button className="cart-btn" disabled={inCart ? true : false} onClick={()=>{ value.addToCart(id);}}>
  
                      {inCart?(<p className="text-capitalize mb-0" disabled>
                      {""}
@@ -28,6 +28,15 @@ export default class Product extends Component {
 
                      </button>
                      </div>
+)}
+
+                
+
+               
+
+                     </ProductConsumer>
+
+
                      {/* card footer */}
                      <div className="card-footer d-flex justify-content-between">
                          <p className="align-self-center mb-0">
@@ -44,6 +53,16 @@ export default class Product extends Component {
         );
     }
 }
+
+Product.propTypes = {
+    product:PropTypes.shape({
+      id: PropTypes.number,
+      img: PropTypes.string,
+      title: PropTypes.string,
+      price: PropTypes.number,
+      inCart: PropTypes.bool  
+  }).isRequired
+};
 
 const ProductWrapper = styled.div`
 .card{
@@ -76,7 +95,7 @@ const ProductWrapper = styled.div`
     transform:scale(1.2);
 }
 
-.card-btn {
+.cart-btn{
     position:absolute;
     bottom:0;
     right: 0;
--- src/context.js
+++ src/context.js
@@ -7,15 +7,51 @@ const ProductContext = React.createContext();
 
 class ProductProvider extends Component {
     state ={
-        products:storeProducts,
-        detailProduct:detailProduct
+        products: [],
+        detailProduct:detailProduct,
+        cart:[]
+    };
+    componentDidMount() {
+        this.setProducts();
     }
-    handleDetail= () => {
-    console.log('hello from detail');
-             }
-             addToCart= () => {
-                console.log('hello from add to cart');
-                        };
+
+    setProducts = () =>{
+        let tempProducts = [];
+        storeProducts.forEach(item =>{
+            const singleItem = {...item};
+            tempProducts = [...tempProducts, singleItem];
+
+        })
+        this.setState(() => {
+            return {products:tempProducts}
+        })
+    } 
+
+    getItem = (id) =>{
+       const product = this.state.products.find(item => item.id === id);
+       return product;
+    }
+
+    handleDetail= (id) => {
+       const product = this.getItem(id);
+       this.setState(()=>{
+           return {detailProduct:product}
+       })
+    };
+
+             addToCart= id => {
+                let tempProducts = [...this.state.products];
+                const index = tempProducts.indexOf(this.getItem(id));
+                const product = tempProducts[index];
+                product.inCart = true;
+                product.count = 1;
+                const price = product.price;
+                product.total = price;
+                this.setState(()=>{
+                    return {products:tempProducts, cart:[...this.state.cart,] };
+                });
+             };
+
     render() {
         return (
             <ProductContext.Provider value={{
